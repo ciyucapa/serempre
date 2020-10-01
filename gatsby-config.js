8dec6a2136a4e478ad613e13d5b6d5dd6f5d7187
ciyucapa <cindycaceres134@gmail.com>
2020-10-01 07:41:26 -0500

    Se removio gatsby-source-strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,6 @@ module.exports = {
   plugins: [
       `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-source-strapi`,
-      options: {
-        apiURL: 'https://api-7ri8d.strapidemo.com',
-        queryLimit: 1000,
-        contentTypes: [`tasks`],
-      },
-    },
-    {
       resolve: 'gatsby-plugin-apollo',
       options: {
         uri: `https://api-7ri8d.strapidemo.com/graphql`
